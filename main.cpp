53bef8ddcfa78e019c37f63ed29e259100b14a77
shenzilun <shenzilun@gmail.com>
2016-12-04 20:15:42 -0500

    create main.cpp and revise gitignore

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * main.cpp: This is the entrance of the trading system
+ * TODO: Main functions of this trading system
+ *
+ * @author Zilun Shen, shenzilun@gmail.com
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "hello world" << endl;
+	return 0;
+}
+
